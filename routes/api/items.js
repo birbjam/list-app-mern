17a6f5a964e7bfd2260047cb7e9f727603552564
birbjam <41635757+birbjam@users.noreply.github.com>
2019-10-04 15:23:56 -0700

    GET POST and DELETE routes established, successfully added and deleted from the db

--- routes/api/items.js
+++ routes/api/items.js
@@ -14,4 +14,25 @@ router.get('/', (req, res) => {
     .catch(err => console.log(err))
 })
 
+// @route POST api/items
+// @ desc Create An Item
+// @access Public
+router.post('/', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  })
+  newItem.save().then(item => res.json(item))
+    .catch(err => console.log(err))
+})
+
+// @route DELETE api/items/:id
+// @ desc Delete An Item
+// @access Public
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove()
+      .then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }));
+})
+
 module.exports = router
