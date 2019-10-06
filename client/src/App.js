b293a5ad34623057b48d6a42e6b9eabb8e9939f0
birbjam <41635757+birbjam@users.noreply.github.com>
2019-10-06 14:27:50 -0700

    added static list items and transition for the delete button.

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import AppNavbar from './components/AppNavbar'
+import List from './components/List'
 
 function App() {
   return (
     <div className="App">
       <AppNavbar />
+      <List />
     </div>
   );
 }
---
+++ client/src/components/List.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import uuid from 'uuid'
+
+class List extends Component {
+  state = {
+    items: [
+      { id: uuid(), name: "Stuff" },
+      { id: uuid(), name: "Things" },
+      { id: uuid(), name: "Carrots" },
+      { id: uuid(), name: "Chungus" }
+    ]
+  };
+
+  render() {
+      const { items } = this.state
+      return(
+          <Container>
+              <Button
+              color="dark"
+              style={{marginBottom: '2rem'}}
+              onClick={() => {
+                  const name = prompt('Enter Item')
+                  if(name) {
+                      this.setState(state => ({
+                          items:[...state.items, { id: uuid(), name}]
+                      }))
+                  }
+              }}> Add Item</Button>
+
+              <ListGroup>
+                  <TransitionGroup className='list'>
+                      {items.map(({ id, name }) => (
+                          <CSSTransition key={id} timeout={500} classNames='fade'>
+                              <ListGroupItem>
+                              <Button
+                              className='remove-btn' 
+                              color='danger' 
+                              size='sm' 
+                              marginRight={0.5}
+                              onClick={() => {
+                                  this.setState(state => ({
+                                      items: state.items.filter(item => item.id !== id)
+                                  }))
+                              }}
+                              >&times;</Button>
+                                  {name}
+                              </ListGroupItem>
+                          </CSSTransition>
+                      ))}
+                  </TransitionGroup>
+              </ListGroup>
+          </Container>
+      )
+  }
+}
+
+export default List
