7da87d6e23c86e0656ba9bdc6f930201ec051d2a
birbjam <41635757+birbjam@users.noreply.github.com>
2019-10-08 16:10:57 -0700

    added redux

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import AppNavbar from './components/AppNavbar'
 import List from './components/List'
+import { Provider } from 'react-redux'
+import store from './store'
 
 function App() {
   return (
-    <div className="App">
-      <AppNavbar />
-      <List />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
+        <List />
+      </div>
+    </Provider>
   );
 }
 
--- client/src/components/List.js
+++ client/src/components/List.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import uuid from 'uuid'
+import { connect } from 'react-redux'
+import { getItems } from '../actions/itemActions'
 
 class List extends Component {
   state = {
